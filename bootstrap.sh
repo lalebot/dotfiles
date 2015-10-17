603da502b21d56f92e4c898cb27dad15321877c5
lalebot <ale_pistilli@yahoo.com.ar>
2015-10-17 20:35:49 -0300

    modifico menu

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,17 @@ ln -s ~/git/dotfiles/config.fish ~/.config/fish/config.fish
 rm ~/.config/openbox/rc.xml
 ln -s ~/git/dotfiles/openbox/rc.xml ~/.config/openbox/rc.xml
 rm ~/.config/openbox/menu.xml
-ln -s ~/git/dotfiles/openbox/rc.xml ~/.config/openbox/menu.xml
+ln -s ~/git/dotfiles/openbox/menu.xml ~/.config/openbox/menu.xml
+mkdir ~/.config/openbox/pipemenus
+rm ~/.config/openbox/pipemenus/obpipemenu-places
+ln -s ~/git/dotfiles/openbox/pipemenus/obpipemenu-places ~/.config/openbox/pipemenus/obpipemenu-places
+rm ~/.config/openbox/pipemenus/fehpipe
+ln -s ~/git/dotfiles/openbox/pipemenus/fehpipe ~/.config/openbox/pipemenus/fehpipe
+rm ~/.config/openbox/pipemenus/obrecent.sh
+ln -s ~/git/dotfiles/openbox/pipemenus/obrecent.sh ~/.config/openbox/pipemenus/obrecent.sh
+rm ~/.config/obmenu-generator/config.pl
+ln -s ~/git/dotfiles/openbox/obmenu-generator/config.pl ~/.config/obmenu-generator/config.pl
+rm ~/.config/obmenu-generator/schema.pl
+ln -s ~/git/dotfiles/openbox/obmenu-generator/schema.pl ~/.config/obmenu-generator/schema.pl
 rm ~/.config/gtk-3.0/gtk.css
 ln -s ~/git/dotfiles/gtk3.css ~/.config/gtk-3.0/gtk.css
