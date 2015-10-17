8587ad68c92a246474fde984d121d3e90d11ff6e
lalebot <ale_pistilli@yahoo.com.ar>
2015-10-17 19:58:05 -0300

    modifico menu

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 rm ~/.vimrc
-rm ~/.config/terminator/config
-rm ~/.config/fish/config.fish
 ln -s ~/git/dotfiles/vimrc ~/.vimrc
+rm ~/.config/terminator/config
 ln -s ~/git/dotfiles/config-terminator ~/.config/terminator/config
+rm ~/.config/fish/config.fish
 ln -s ~/git/dotfiles/config.fish ~/.config/fish/config.fish
 rm ~/.config/openbox/rc.xml
 ln -s ~/git/dotfiles/openbox/rc.xml ~/.config/openbox/rc.xml
+rm ~/.config/openbox/menu.xml
+ln -s ~/git/dotfiles/openbox/rc.xml ~/.config/openbox/menu.xml
+rm ~/.config/gtk-3.0/gtk.css
+ln -s ~/git/dotfiles/gtk3.css ~/.config/gtk-3.0/gtk.css
